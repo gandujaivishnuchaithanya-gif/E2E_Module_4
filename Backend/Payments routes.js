3dc8a12895c2377775c44a56315c511aa79ad36b
gandujaivishnuchaithanya-gif <gandujaivishnuchaithanya@gmail.com>
2026-02-20 09:24:41 +0530

    Add payment routes for creating and retrieving payments

--- Backend/Payments routes.js
+++ Backend/Payments routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/paymentController');
+
+router.post('/', paymentController.createPayment);
+router.get('/', paymentController.getPayments);
+
+module.exports = router;

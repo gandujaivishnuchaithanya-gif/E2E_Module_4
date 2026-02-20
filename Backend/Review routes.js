ef93c28c7e138eef8d76cd3d16086e353573d525
gandujaivishnuchaithanya-gif <gandujaivishnuchaithanya@gmail.com>
2026-02-20 09:25:20 +0530

    Add review routes for creating and fetching reviews

--- Backend/Review routes.js
+++ Backend/Review routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+const reviewController = require('../controllers/reviewController');
+
+router.post('/', reviewController.createReview);
+router.get('/', reviewController.getReviews);
+
+module.exports = router;
